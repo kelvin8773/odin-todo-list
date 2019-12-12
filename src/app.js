3d4f3a3fdb8a141cce1b2a38a13cacea9a5d0b20
Kelvin Liang <kelvin9877@gmail.com>
2019-12-12 19:19:55 +0800

    integrate bootstrap & fontawesome

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
+import './style.css';
+
 console.log('Test ToDo List!')

483e1ad0fd85316f1f0449eb86f182ec67119171
Abdusaid10 <abdusaidabdurasulov@gmail.com>
2019-12-16 20:16:39 +0500

    tried to change linter errors

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+ 
+    "parserOptions": { "ecmaVersion": 6 },
+    "extends": "airbnb-base",
+        rules: {
+        'no-shadow': 0,
+        'no-param-reassign': 0,
+        'eol-last': 0,
+    }
+    
+      
+};
